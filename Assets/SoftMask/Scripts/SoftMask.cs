5926414ff11d6baa67b68a8e4dc2e28f7758223d
Oleg Knyazev <knyazev751@gmail.com>
2016-12-19 19:01:10 +0300

    Add automatic instantiation of hidden SoftMaskables on all children on SoftMask

---
+++ Assets/SoftMask/Scripts/SoftMask.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace SoftMask {
+    [ExecuteInEditMode]
+    [AddComponentMenu("UI/Soft Mask", 14)]
+    public class SoftMask : UIBehaviour {
+        protected virtual void Update() {
+            SpawnMaskablesInChildren();
+        }
+
+        void SpawnMaskablesInChildren() {
+            foreach (var g in transform.GetComponentsInChildren<Graphic>())
+                if (!g.GetComponent<SoftMaskable>())
+                    g.gameObject.AddComponent<SoftMaskable>();
+        }
+    }
+}
---
+++ Assets/SoftMask/Scripts/SoftMaskable.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SoftMask {
+    [ExecuteInEditMode]
+    [AddComponentMenu("")]
+    public class SoftMaskable : UIBehaviour {
+        SoftMask _mask;
+
+        protected override void Awake() {
+            base.Awake();
+            hideFlags = HideFlags.HideAndDontSave;
+            FindMask();
+            print("SoftMaskable.Awake " + GetInstanceID());
+        }
+        
+        protected override void OnDestroy() {
+            base.OnDestroy();
+            print("SoftMaskable.OnDestroy " + GetInstanceID());
+        }
+
+        protected override void OnTransformParentChanged() {
+            base.OnTransformParentChanged();
+            FindMask();
+            if (!_mask)
+                DestroyImmediate(this);
+        }
+
+        void FindMask() {
+            _mask = GetComponentInParent<SoftMask>();
+        }
+    }
+}
