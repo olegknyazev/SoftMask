6836f0dd39e73db8c6e16be78f63dff46bf498fd
Oleg Knyazev <knyazev751@gmail.com>
2016-12-19 20:17:24 +0300

    Implement IMaterialModifier by SoftMaskable and add materials registry to SoftMask

--- Assets/SoftMask/Scripts/SoftMask.cs
+++ Assets/SoftMask/Scripts/SoftMask.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
@@ -6,14 +9,91 @@ namespace SoftMask {
     [ExecuteInEditMode]
     [AddComponentMenu("UI/Soft Mask", 14)]
     public class SoftMask : UIBehaviour {
+        readonly List<MaterialOverride> _overrides = new List<MaterialOverride>();
+        [SerializeField] Shader _defaultMaskShader;
+
+        public Shader defaultMaskShader { get; set; }
+
         protected virtual void Update() {
             SpawnMaskablesInChildren();
+        }
+
+        protected override void OnDestroy() {
+            base.OnDestroy();
+            DestroyAllOverrides();
         }
 
         void SpawnMaskablesInChildren() {
             foreach (var g in transform.GetComponentsInChildren<Graphic>())
                 if (!g.GetComponent<SoftMaskable>())
                     g.gameObject.AddComponent<SoftMaskable>();
+        }
+
+        // May return null.
+        public Material GetReplacement(Material original) {
+            for (int i = 0; i < _overrides.Count; ++i) {
+                var entry = _overrides[i];
+                if (entry.original == original)
+                    return entry.Get();
+            }
+            var replacement = Replace(original);
+            if (replacement)
+                replacement.hideFlags = HideFlags.HideAndDontSave;
+            _overrides.Add(new MaterialOverride(original, replacement));
+            return replacement;
+        }
+
+        public void ReleaseReplacement(Material replacement) {
+            for (int i = 0; i < _overrides.Count; ++i) {
+                var entry = _overrides[i];
+                if (entry.replacement == replacement)
+                    if (entry.Release()) {
+                        DestroyImmediate(replacement);
+                        return;
+                    }   
+            }
+        }
+
+        void DestroyAllOverrides() {
+            for (int i = 0; i < _overrides.Count; ++i)
+                DestroyImmediate(_overrides[i].replacement);
+            _overrides.Clear();
+        }
+
+        Material Replace(Material original) {
+            if (original == null)
+                return new Material(_defaultMaskShader);
+            else if (original == Canvas.GetDefaultCanvasMaterial())
+                return new Material(_defaultMaskShader);
+            else if (original == Canvas.GetDefaultCanvasTextMaterial())
+                throw new NotSupportedException();
+            else if (original.HasProperty("_SoftMask"))
+                return new Material(original);
+            else
+                return null;
+        }
+
+        class MaterialOverride {
+            int _useCount;
+
+            public MaterialOverride(Material original, Material replacement) {
+                this.original = original;
+                this.replacement = replacement;
+                _useCount = 1;
+            }
+
+            public Material original { get; private set; }
+            public Material replacement { get; private set; }
+
+            public Material Get() {
+                ++_useCount;
+                return replacement;
+            }
+
+            public bool Release() {
+                Assert.IsTrue(_useCount > 0);
+                return --_useCount == 0;
+            }
         }
     }
 }
--- Assets/SoftMask/Scripts/SoftMaskable.cs
+++ Assets/SoftMask/Scripts/SoftMaskable.cs
@@ -1,33 +1,85 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace SoftMask {
     [ExecuteInEditMode]
     [AddComponentMenu("")]
-    public class SoftMaskable : UIBehaviour {
+    public class SoftMaskable : UIBehaviour, IMaterialModifier {
         SoftMask _mask;
+        Graphic _graphic;
+        bool _warned;
+        Material _material;
+
+        public Material GetModifiedMaterial(Material baseMaterial) {
+            if (_mask) {
+                // We should find replacemnt first and only then Release a previous one.
+                // It allows to not delete the old material if it may be reused.
+                var replacement = _mask.GetReplacement(baseMaterial);
+                material = replacement;
+                if (material) {
+                    _warned = false;
+                    return material;
+                }   
+                WarnMaskingWillNotWork(baseMaterial);
+            }
+            return baseMaterial;
+        }
 
         protected override void Awake() {
             base.Awake();
             hideFlags = HideFlags.HideAndDontSave;
-            FindMask();
-            print("SoftMaskable.Awake " + GetInstanceID());
+            mask = FindMask();
         }
         
         protected override void OnDestroy() {
             base.OnDestroy();
-            print("SoftMaskable.OnDestroy " + GetInstanceID());
+            mask = null;
         }
 
         protected override void OnTransformParentChanged() {
             base.OnTransformParentChanged();
-            FindMask();
-            if (!_mask)
+            mask = FindMask();
+            if (!mask)
                 DestroyImmediate(this);
         }
 
-        void FindMask() {
-            _mask = GetComponentInParent<SoftMask>();
+        Graphic graphic { get { return _graphic ?? (_graphic = GetComponent<Graphic>()); } }
+
+        Material material {
+            get { return _material; }
+            set {
+                if (_material != value) {
+                    if (_material && _mask)
+                        _mask.ReleaseReplacement(_material);
+                    _material = value;
+                }
+            }
+        }
+
+        SoftMask mask {
+            get { return _mask; }
+            set {
+                if (_mask != value) {
+                    if (_mask)
+                        material = null;
+                    _mask = value;
+                }
+            }
+        }
+
+        SoftMask FindMask() { return GetComponentInParent<SoftMask>(); }
+
+        void WarnMaskingWillNotWork(Material material) {
+            if (!_warned) {
+                Debug.LogWarningFormat(
+                    "Soft Mask will not work on {0} because material {1} doesn't support masking. " +
+                    "Add masking support to your material or set Graphic's material to None to use " +
+                    "a default one.",
+                    graphic,
+                    material);
+                _warned = true;
+            }
         }
     }
 }
