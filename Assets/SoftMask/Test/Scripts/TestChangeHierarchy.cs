0341ab8b840dbb6b58cee01213ec04c2a6efe05c
Oleg Knyazev <knyazev751@gmail.com>
2016-12-24 15:24:08 +0300

    Add test of hierarchy changing at play time

---
+++ Assets/SoftMask/Test/Scripts/TestChangeHierarchy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TestChangeHierarchy : MonoBehaviour {
+    public RectTransform panelWithMask;
+    public RectTransform panelWithoutMask;
+    public RectTransform[] objects;
+
+    public IEnumerator Start() {
+        while (true) {
+            for (int i = 0; i < objects.Length; ++i) {
+                var obj = objects[i];
+                obj.transform.SetParent(AnotherPanel(obj.transform.parent.GetComponent<RectTransform>()), false);
+                yield return new WaitForSeconds(0.5f);
+            }
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    RectTransform AnotherPanel(RectTransform current) {
+        return current == panelWithMask ? panelWithoutMask : panelWithMask;
+    }
+}
