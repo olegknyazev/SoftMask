a9ccc8143a83bf4d6bae36b4bf758689b3f490e7
Oleg Knyazev <knyazev751@gmail.com>
2017-01-04 23:49:52 +0300

    Add test of dynamically creating and destroying SoftMask component (it doesn't work yet)

---
+++ Assets/SoftMask/Test/Scripts/TestCreateAndDestroyMask.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TestCreateAndDestroyMask : MonoBehaviour {
+    public GameObject panel;
+
+    public IEnumerator Start() {
+        SoftMask.SoftMask mask = null;
+        while (true) {
+            if (mask) {
+                DestroyImmediate(mask);
+                mask = null;
+            } else {
+                mask = panel.AddComponent<SoftMask.SoftMask>();
+            }
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
