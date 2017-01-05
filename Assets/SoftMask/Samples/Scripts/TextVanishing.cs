fb26c314be2713607e18f652b357a047e68f4325
Oleg Knyazev <knyazev751@gmail.com>
2017-01-05 19:03:45 +0300

    Rename Demo to Samples and add checkbox to the first sample

---
+++ Assets/SoftMask/Samples/Scripts/TextVanishing.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SoftMasking.Samples {
+    public class TextVanishing : MonoBehaviour {
+        public GameObject viewport;
+
+        public void ToggleSoftMask(bool enabled) {
+            viewport.GetComponent<SoftMask>().enabled = enabled;
+            viewport.GetComponent<Image>().enabled = !enabled;
+            viewport.GetComponent<Mask>().enabled = !enabled;
+        }
+    }
+}
