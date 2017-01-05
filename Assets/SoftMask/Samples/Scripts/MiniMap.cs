11e73e7a8e6c5f3eb1e84b09bd6322f28d02e8a3
Oleg Knyazev <knyazev751@gmail.com>
2017-01-05 20:39:35 +0300

    Add the second sample with minimap and move to Unity 5.5

---
+++ Assets/SoftMask/Samples/Scripts/MiniMap.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace SoftMasking.Samples {
+    public class MiniMap : MonoBehaviour {
+        public RectTransform map;
+        public RectTransform marker;
+
+        public void LateUpdate() {
+            map.anchoredPosition = -marker.anchoredPosition;
+        }
+    }
+}
