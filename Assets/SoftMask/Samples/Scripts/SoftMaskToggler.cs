202de90f83c37ae72766fafb1483d30c79196f3f
Oleg Knyazev <knyazev751@gmail.com>
2017-01-06 13:39:42 +0300

    Add an ability to scroll the view by dragging empty space in the third sample

--- Assets/SoftMask/Samples/Scripts/SoftMaskToggler.cs
+++ Assets/SoftMask/Samples/Scripts/SoftMaskToggler.cs
@@ -4,11 +4,13 @@ using UnityEngine.UI;
 namespace SoftMasking.Samples {
     public class SoftMaskToggler : MonoBehaviour {
         public GameObject viewport;
+        public bool doNotTouchImage = false;
 
         public void Toggle(bool enabled) {
             viewport.GetComponent<SoftMask>().enabled = enabled;
-            viewport.GetComponent<Image>().enabled = !enabled;
             viewport.GetComponent<Mask>().enabled = !enabled;
+            if (!doNotTouchImage)
+                viewport.GetComponent<Image>().enabled = !enabled;
         }
     }
 }
