067594fd873cebd26a2917c3c724e7396807eebe
Oleg Knyazev <knyazev751@gmail.com>
2017-01-07 21:30:03 +0300

    MiniMapAligner renamed to more generic MovementCompensator

---
+++ Assets/SoftMask/Samples/Scripts/MovementCompensator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace SoftMasking.Samples {
+    public class MovementCompensator : MonoBehaviour {
+        public RectTransform content;
+        public RectTransform viewport;
+
+        public void LateUpdate() {
+            content.anchoredPosition = -viewport.anchoredPosition;
+        }
+    }
+}
