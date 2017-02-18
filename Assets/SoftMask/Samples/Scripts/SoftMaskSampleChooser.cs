5c872a8f35f77a9ec30db38cbea196556aed16ce
Oleg Knyazev <knyazev751@gmail.com>
2017-02-18 20:34:14 +0300

    Add replacing of the sample switcher with a static text in case when scenes weren't added to build

--- Assets/SoftMask/Samples/Scripts/SoftMaskSampleChooser.cs
+++ Assets/SoftMask/Samples/Scripts/SoftMaskSampleChooser.cs
@@ -1,13 +1,27 @@
-﻿using UnityEngine;
+﻿using System.IO;
+using System.Linq;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace SoftMasking.Samples {
     public class SoftMaskSampleChooser : MonoBehaviour {
         public Dropdown dropdown;
+        public Text fallbackLabel;
 
         public void Start() {
             var activeSceneName = SceneManager.GetActiveScene().name;
+#if UNITY_EDITOR
+            dropdown.options.RemoveAll(x => !IsSceneInBuild(x.text));
+            if (dropdown.options.Count == 0) {
+                dropdown.gameObject.SetActive(false);
+                fallbackLabel.gameObject.SetActive(true);
+                fallbackLabel.text = activeSceneName;
+            }
+#endif
             dropdown.value = dropdown.options.FindIndex(x => x.text == activeSceneName);
             dropdown.onValueChanged.AddListener(Choose);
         }
@@ -16,5 +30,14 @@ namespace SoftMasking.Samples {
             var sceneName = dropdown.options[sampleIndex].text;
             SceneManager.LoadScene(sceneName);
         }
+
+        
+#if UNITY_EDITOR
+        bool IsSceneInBuild(string sceneName) {
+            return 
+                EditorBuildSettings.scenes.Any(
+                    s => Path.GetFileNameWithoutExtension(s.path) == sceneName);
+        }
+#endif
     }
 }
