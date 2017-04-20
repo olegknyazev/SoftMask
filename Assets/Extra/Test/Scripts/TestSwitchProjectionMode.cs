ed5d2ed84b0cf4582b9cd3c073e9b1bec0bf9a4a
Oleg Knyazev <knyazev751@gmail.com>
2017-04-20 19:06:42 +0300

    Add a test with a canvas moving in 3D space and switching of the camera projection mode

---
+++ Assets/Extra/Test/Scripts/TestSwitchProjectionMode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace SoftMasking.Tests {
+    public class TestSwitchProjectionMode : MonoBehaviour {
+        public Camera testCamera;
+
+        public IEnumerator Start() {
+            while (true) {
+                yield return new WaitForSeconds(2);
+                testCamera.orthographic = !testCamera.orthographic;
+            }
+        }
+    }
+}
