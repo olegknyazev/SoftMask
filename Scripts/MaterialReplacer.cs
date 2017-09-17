fc887f426635dba054bc10cf398a36d36a077b6a
Oleg Knyazev <knyazev751@gmail.com>
2017-09-17 12:08:07 +0300

    Document code that is related to automatic material replacement

--- Scripts/MaterialReplacer.cs
+++ Scripts/MaterialReplacer.cs
@@ -4,22 +4,51 @@ using System.Linq;
 using UnityEngine;
 
 namespace SoftMasking {
+    /// <summary>
+    /// Mark an implementation of IMaterialReplacer interface with this attribute to
+    /// register it in the global material replacer chain. These replacers will be used
+    /// automatically by all SoftMasks.
+    ///
+    /// Globally registered replacers are called in order of ascending their `order`
+    /// value. The traversal is stopped on the first IMaterialReplacer which returns
+    /// non-null value and its returned value is used as a replacement.
+    ///
+    /// Implementation of IMaterialReplacer marked by this attribute should have a
+    /// default constructor.
+    /// </summary>
+    /// <seealso cref="IMaterialReplacer"/>
+    /// <seealso cref="MaterialReplacer.globalReplacers"/>
     [AttributeUsage(AttributeTargets.Class)]
     public class GlobalMaterialReplacerAttribute : Attribute { }
 
+    /// <summary>
+    /// Used by SoftMask to automatically replace materials which don't support Soft Mask
+    /// by those that do.
+    /// </summary>
+    /// <seealso cref="GlobalMaterialReplacerAttribute"/>
     public interface IMaterialReplacer {
-        // Determines order in which IMaterialReplacer will be called. Order of default
-        // implementation is 0. If you want your function to be called before, return a
-        // value lesser than 0.
+        /// <summary>
+        /// Determines mutual order in which IMaterialReplacers will be called. The
+        /// lesser the return value, the earlier it will be called, that is,
+        /// IMaterialReplacers are sorted by ascending of the order.
+        /// The order of default implementation is 0. If you want your function to be
+        /// called before, return a value lesser than 0.
+        /// </summary>
         int order { get; }
 
-        // Should return null if this replacer can't replace given material.
+        /// <summary>
+        /// Should return null if this replacer can't replace the given material.
+        /// </summary>
         Material Replace(Material material);
     }
 
     public static class MaterialReplacer {
         static List<IMaterialReplacer> _globalReplacers;
 
+        /// <summary>
+        /// Returns the collection of all globally registered replacers.
+        /// </summary>
+        /// <seealso cref="GlobalMaterialReplacerAttribute"/>
         public static IEnumerable<IMaterialReplacer> globalReplacers {
             get {
                 if (_globalReplacers == null)
