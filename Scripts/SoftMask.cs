35bc359aa8c19e482fb70b1fa15d3e264228615a
Oleg Knyazev <knyazev751@gmail.com>
2018-10-07 18:09:54 +0300

    Update the documentation to version 1.3.1

--- Scripts/SoftMask.cs
+++ Scripts/SoftMask.cs
@@ -27,7 +27,7 @@ namespace SoftMasking {
     [DisallowMultipleComponent]
     [AddComponentMenu("UI/Soft Mask", 14)]
     [RequireComponent(typeof(RectTransform))]
-    [HelpURL("https://docs.google.com/document/d/1MavdgKorCTqP4yMPeGMXSUY7BOXknwlGaFCxXXW0QvU")]
+    [HelpURL("https://docs.google.com/document/d/1XhJFNFHNyKXwWsErLkd1FBw0YgOCeo4qkjrMW9_H-hc")]
     public class SoftMask : UIBehaviour, ISoftMask, ICanvasRaycastFilter {
         //
         // How it works:
