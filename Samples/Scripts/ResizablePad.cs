ea3d0892c6d852f02b9f93b42ec3488f06383bf8
Oleg Knyazev <knyazev751@gmail.com>
2019-04-22 21:50:48 +0300

    Add horizontal and vertical resizing to the inverted mask sample

--- Samples/Scripts/ResizablePad.cs
+++ Samples/Scripts/ResizablePad.cs
@@ -18,7 +18,7 @@ namespace SoftMasking.Samples {
         ManipulationType _activeManipulation;
 
         public float resizeGripSize = 40f;
-        public float rotateGripSize = 60f;
+        public float rotateGripSize = 40f;
 
         protected override void Awake() {
             base.Awake();
@@ -31,20 +31,41 @@ namespace SoftMasking.Samples {
         }
         
         public void OnDrag(PointerEventData eventData) {
+            var prevLocalPoint = ToLocal(eventData.position - eventData.delta, eventData.pressEventCamera);
+            var curLocalPoint = ToLocal(eventData.position, eventData.pressEventCamera);
+            var localDelta = curLocalPoint - prevLocalPoint;
             switch (_activeManipulation) {
                 case ManipulationType.Move:
                     _rectTransform.anchoredPosition += eventData.delta;
                     break;
                 case ManipulationType.Rotate:
                     // TODO reduce copypaste
-                    var prevLever = ToLocal(eventData.position - eventData.delta, eventData.pressEventCamera);
-                    var curLever = ToLocal(eventData.position, eventData.pressEventCamera);
-                    var prevAngle = Mathf.Atan2(prevLever.y, prevLever.x) * Mathf.Rad2Deg;
-                    var curAngle = Mathf.Atan2(curLever.y, curLever.x) * Mathf.Rad2Deg;
+                    var prevAngle = Mathf.Atan2(prevLocalPoint.y, prevLocalPoint.x) * Mathf.Rad2Deg;
+                    var curAngle = Mathf.Atan2(curLocalPoint.y, curLocalPoint.x) * Mathf.Rad2Deg;
                     _rectTransform.localRotation *= Quaternion.AngleAxis(Mathf.DeltaAngle(prevAngle, curAngle), Vector3.forward);
+                    break;
+                case ManipulationType.ResizeLeft:
+                    DirectedResize(HorizontalProjection(localDelta), -1f);
+                    break;
+                case ManipulationType.ResizeUp:
+                    DirectedResize(VerticalProjection(localDelta), -1f);
+                    break;
+                case ManipulationType.ResizeRight:
+                    DirectedResize(HorizontalProjection(localDelta), +1f);
+                    break;
+                case ManipulationType.ResizeDown:
+                    DirectedResize(VerticalProjection(localDelta), +1f);
                     break;
             }
         }
+
+        void DirectedResize(Vector2 localResizeDelta, float direction) { // TODO it's unclear what `direction` is
+            _rectTransform.sizeDelta += localResizeDelta * direction;
+            _rectTransform.position += _rectTransform.TransformVector(localResizeDelta) / 2;
+        }
+
+        Vector2 HorizontalProjection(Vector2 vec) { return new Vector2(vec.x, 0f); }
+        Vector2 VerticalProjection(Vector2 vec) { return new Vector2(0f, vec.y); }
 
         public void OnEndDrag(PointerEventData eventData) {
             _activeManipulation = ManipulationType.None;
@@ -62,12 +83,13 @@ namespace SoftMasking.Samples {
                 return ManipulationType.Move;
             else if (InsideRotationZone(pressPosition))
                 return ManipulationType.Rotate;
-            else
-                return ManipulationType.None;
+            else // TODO resize by corner too?
+                 // TODO offset rotation grip a bit outside?
+                return MovementManipulationBySector(pressPosition);
         }
 
         bool InsideMovementZone(Vector2 pos) {
-            var grip = resizeGripSize / 2;
+            var grip = resizeGripSize;
             var selfRect = _rectTransform.rect;
             return pos.x > selfRect.xMin + grip
                 && pos.x < selfRect.xMax - grip
@@ -84,8 +106,16 @@ namespace SoftMasking.Samples {
         }
 
         Rect RotationZone(Vector2 anchor) {
-            var grip = rotateGripSize / 2;
+            var grip = rotateGripSize;
             return new Rect(anchor.x - grip, anchor.y - grip, rotateGripSize, rotateGripSize);
+        }
+
+        ManipulationType MovementManipulationBySector(Vector2 pos) {
+            var normalizedPos = new Vector2(pos.x / _rectTransform.rect.width, pos.y / _rectTransform.rect.height);
+            if (Mathf.Abs(normalizedPos.x) > Mathf.Abs(normalizedPos.y))
+                return normalizedPos.x >= 0f ? ManipulationType.ResizeRight : ManipulationType.ResizeLeft;
+            else
+                return normalizedPos.y >= 0f ? ManipulationType.ResizeDown : ManipulationType.ResizeUp;
         }
     }
 }
