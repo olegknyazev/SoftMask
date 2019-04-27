4894b79c9b5b6f5b8ee989d3f9807ee3fd18e69e
Oleg Knyazev <knyazev751@gmail.com>
2019-04-27 12:34:25 +0300

    Add display of two-side resizings (top-left, top-right, etc.)

---
+++ Samples/Scripts/HighlightOnSelect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace SoftMasking.Samples {
+    [RequireComponent(typeof(RectTransform))]
+    public class HighlightOnSelect : UIBehaviour, IPointerEnterHandler, IPointerExitHandler {
+        public Graphic[] targetGraphics;
+
+        public void OnPointerEnter(PointerEventData eventData) {
+            foreach (var graphic in targetGraphics)
+                graphic.CrossFadeAlpha(1f, 0.2f, true);
+        }
+
+        public void OnPointerExit(PointerEventData eventData) {
+            foreach (var graphic in targetGraphics)
+                graphic.CrossFadeAlpha(0f, 0.2f, true);
+        }
+    }
+}
