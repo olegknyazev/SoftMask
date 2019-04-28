5716f8a91f1131a237a04da64f563206be7fcccb
Oleg Knyazev <knyazev751@gmail.com>
2019-04-28 11:09:04 +0300

    Reimplement movement and resizing in a single object (again)
    Currently without highlighting of the active manipulation mode.

--- Samples/Scripts/RectManipulator.cs
+++ Samples/Scripts/RectManipulator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -22,35 +23,67 @@ namespace SoftMasking.Samples {
         ManipulationType _activeManipulation;
 
         public RectTransform targetTransform;
-        public ManipulationType manipulation;
-        
+        public float resizeGripSize;
+
         public void OnBeginDrag(PointerEventData eventData) {
-            _activeManipulation = manipulation;
+            _activeManipulation = ManipulationByPressPosition(ToLocal(eventData.pressPosition, eventData.pressEventCamera));
         }
         
+        ManipulationType ManipulationByPressPosition(Vector2 pos) {
+            var rect = targetTransform.rect;
+            var xSegment = DetermineSegment(pos.x, rect.xMin, rect.xMax, resizeGripSize);
+            var ySegment = DetermineSegment(pos.y, rect.yMin, rect.yMax, resizeGripSize);
+            ManipulationType result;
+            return manipulationBySegment.TryGetValue(new SegmentPair(xSegment, ySegment), out result)
+                ? result
+                : ManipulationType.None;
+        }
+
+        Segment DetermineSegment(float v, float min, float max, float border) {
+            if (v >= min && v < min + border)
+                return Segment.Lower;
+            else if (v >= min + border && v < max - border)
+                return Segment.Center;
+            else if (v >= max - border && v < max)
+                return Segment.Upper;
+            else
+                return Segment.None;
+        }
+        
+        enum Segment { None, Lower, Center, Upper }
+
+        struct SegmentPair {
+            public Segment x;
+            public Segment y;
+            public SegmentPair(Segment x_, Segment y_) {
+                x = x_;
+                y = y_;
+            }
+        }
+
+        static readonly Dictionary<SegmentPair, ManipulationType> manipulationBySegment =
+            new Dictionary<SegmentPair, ManipulationType>() {
+                { new SegmentPair(Segment.Lower, Segment.Upper), ManipulationType.ResizeUpLeft },
+                { new SegmentPair(Segment.Center, Segment.Upper), ManipulationType.ResizeUp},
+                { new SegmentPair(Segment.Upper, Segment.Upper), ManipulationType.ResizeUpRight},
+
+                { new SegmentPair(Segment.Lower, Segment.Center), ManipulationType.ResizeLeft },
+                { new SegmentPair(Segment.Center, Segment.Center), ManipulationType.Move},
+                { new SegmentPair(Segment.Upper, Segment.Center), ManipulationType.ResizeRight},
+
+                { new SegmentPair(Segment.Lower, Segment.Lower), ManipulationType.ResizeDownLeft },
+                { new SegmentPair(Segment.Center, Segment.Lower), ManipulationType.ResizeDown},
+                { new SegmentPair(Segment.Upper, Segment.Lower), ManipulationType.ResizeDownRight },
+            };
+
         public void OnDrag(PointerEventData eventData) {
             if (targetTransform == null)
                 return;
-            Func<ManipulationType, bool> Is = ActiveManipulationIs;
             var prevLocalPoint = ToLocal(eventData.position - eventData.delta, eventData.pressEventCamera);
             var curLocalPoint = ToLocal(eventData.position, eventData.pressEventCamera);
-            if (Is(ManipulationType.Move))
-                targetTransform.anchoredPosition += eventData.delta;
-            if (Is(ManipulationType.Rotate))
-                targetTransform.localRotation *= Quaternion.AngleAxis(DeltaRotation(prevLocalPoint, curLocalPoint), Vector3.forward);
-            var localDelta = curLocalPoint - prevLocalPoint;
-            if (Is(ManipulationType.ResizeLeft))
-                ResizeDirected(HorizontalProjection(localDelta), -1f);
-            if (Is(ManipulationType.ResizeUp))
-                ResizeDirected(VerticalProjection(localDelta), +1f);
-            if (Is(ManipulationType.ResizeRight))
-                ResizeDirected(HorizontalProjection(localDelta), +1f);
-            if (Is(ManipulationType.ResizeDown))
-                ResizeDirected(VerticalProjection(localDelta), -1f);
-        }
-
-        bool ActiveManipulationIs(ManipulationType manipulation) {
-            return (_activeManipulation & manipulation) == manipulation;
+            DoMove(eventData);
+            DoRotate(prevLocalPoint, curLocalPoint);
+            DoResize(curLocalPoint - prevLocalPoint);
         }
 
         Vector2 ToLocal(Vector2 position, Camera eventCamera) {
@@ -58,6 +91,33 @@ namespace SoftMasking.Samples {
             RectTransformUtility.ScreenPointToLocalPointInRectangle(
                 targetTransform, position, eventCamera, out localPosition);
             return localPosition;
+        }
+
+        void DoMove(PointerEventData eventData) {
+            if (ActiveManipulationIs(ManipulationType.Move))
+                targetTransform.anchoredPosition += eventData.delta;
+        }
+
+        void DoRotate(Vector2 prevLocalPoint, Vector2 curLocalPoint) {
+            if (ActiveManipulationIs(ManipulationType.Rotate))
+                targetTransform.localRotation *= Quaternion.AngleAxis(DeltaRotation(prevLocalPoint, curLocalPoint), Vector3.forward);
+        }
+
+        void DoResize(Vector2 resizeDelta) {
+            var hDelta = HorizontalProjection(resizeDelta);
+            if (ActiveManipulationIs(ManipulationType.ResizeLeft))
+                ResizeDirected(hDelta, -1f);
+            else if (ActiveManipulationIs(ManipulationType.ResizeRight))
+                ResizeDirected(hDelta, +1f);
+            var vDelta = VerticalProjection(resizeDelta);
+            if (ActiveManipulationIs(ManipulationType.ResizeUp))
+                ResizeDirected(vDelta, +1f);
+            else if (ActiveManipulationIs(ManipulationType.ResizeDown))
+                ResizeDirected(vDelta, -1f);
+        }
+
+        bool ActiveManipulationIs(ManipulationType manipulation) {
+            return (_activeManipulation & manipulation) == manipulation;
         }
 
         float DeltaRotation(Vector2 prevLocalPoint, Vector2 curLocalPoint) {
