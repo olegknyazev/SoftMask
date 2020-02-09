c6167c701bbcef64f34ce93c0fcc74b6ddab0095
Oleg Knyazev <knyazev751@gmail.com>
2020-02-09 13:32:49 +0300

    Add a first and dirty version of sample on render textures

---
+++ Samples/Scripts/MaskPainter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SoftMasking.Samples {
+    [RequireComponent(typeof(RectTransform))]
+    public class MaskPainter : UIBehaviour, IDragHandler {
+        public RectTransform stroke;
+
+        RectTransform _rectTransform;
+
+        protected override void Awake() {
+            base.Awake();
+            _rectTransform = GetComponent<RectTransform>();
+        }
+
+        protected override void Start() {
+            base.Start();
+            
+        }
+
+        public void OnDrag(PointerEventData eventData) {
+            Vector2 localPosition;
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(_rectTransform, eventData.position, null, out localPosition)) {
+                stroke.anchoredPosition = localPosition;
+            }
+        }
+    }
+}
---
+++ Samples/Scripts/PaintedMask.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SoftMasking.Samples {
+    [RequireComponent(typeof(RectTransform))]
+    public class PaintedMask : UIBehaviour {
+        public Canvas renderCanvas;
+        public Camera renderCamera;
+        public SoftMask targetMask;
+
+        RenderTexture _renderTexture;
+
+        protected override void Start() {
+            base.Start();
+            _renderTexture = new RenderTexture(600, 400, 0, RenderTextureFormat.ARGB32);
+            _renderTexture.Create();
+            renderCamera.targetTexture = _renderTexture;
+            targetMask.renderTexture = _renderTexture;
+        }
+    }
+}
