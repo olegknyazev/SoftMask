80389cdc3b856be098e02a468853612f3d2c7cd9
Oleg Knyazev <knyazev751@gmail.com>
2020-03-08 12:34:55 +0300

    Stretch the stroke during movements to smooth out the trail

--- Samples/Scripts/MaskPainter.cs
+++ Samples/Scripts/MaskPainter.cs
@@ -5,19 +5,18 @@ using UnityEngine.UI;
 namespace SoftMasking.Samples {
     [RequireComponent(typeof(RectTransform))]
     public class MaskPainter : UIBehaviour, IPointerDownHandler, IDragHandler {
-        public Graphic stroke;
+        public Graphic spot;
+        public RectTransform stroke;
         RectTransform _rectTransform;
-        RectTransform _strokeTranform;
 
         protected override void Awake() {
             base.Awake();
             _rectTransform = GetComponent<RectTransform>();
-            _strokeTranform = stroke.GetComponent<RectTransform>();
         }
 
         protected override void Start() {
             base.Start();
-            stroke.enabled = false;
+            spot.enabled = false;
         }
 
         public void OnPointerDown(PointerEventData eventData) {
@@ -25,24 +24,33 @@ namespace SoftMasking.Samples {
         }
 
         public void OnDrag(PointerEventData eventData) {
-            UpdateStrokeByEvent(eventData);
+            UpdateStrokeByEvent(eventData, drawTrail: true);
         }
 
-        void UpdateStrokeByEvent(PointerEventData eventData) {
-            UpdateStrokePosition(eventData.position);
+        void UpdateStrokeByEvent(PointerEventData eventData, bool drawTrail = false) {
+            UpdateStrokePosition(eventData.position, drawTrail);
             UpdateStrokeColor(eventData.button);
         }
 
-        void UpdateStrokePosition(Vector2 screenPosition) {
+        void UpdateStrokePosition(Vector2 screenPosition, bool drawTrail = false) {
             Vector2 localPosition;
             if (RectTransformUtility.ScreenPointToLocalPointInRectangle(_rectTransform, screenPosition, null, out localPosition)) {
-                _strokeTranform.anchoredPosition = localPosition;
-                stroke.enabled = true;
+                var prevPosition = stroke.anchoredPosition;
+                stroke.anchoredPosition = localPosition;
+                if (drawTrail)
+                    SetUpTrail(prevPosition);
+                spot.enabled = true;
             }
         }
 
+        void SetUpTrail(Vector2 prevPosition) {
+            var movement = stroke.anchoredPosition - prevPosition;
+            stroke.localRotation = Quaternion.AngleAxis(Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg, Vector3.forward);
+            spot.rectTransform.offsetMin = new Vector2(-movement.magnitude, 0);
+        }
+
         void UpdateStrokeColor(PointerEventData.InputButton pressedButton) {
-            stroke.materialForRendering.SetInt("_BlendOp", 
+            spot.materialForRendering.SetInt("_BlendOp", 
                 pressedButton == PointerEventData.InputButton.Left
                     ? (int)UnityEngine.Rendering.BlendOp.Add
                     : (int)UnityEngine.Rendering.BlendOp.ReverseSubtract);
