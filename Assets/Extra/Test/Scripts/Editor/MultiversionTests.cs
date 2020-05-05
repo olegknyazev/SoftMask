b865a45048fd3b69e24109823468b12a3e4d4a63
Oleg Knyazev <knyazev751@gmail.com>
2020-05-05 15:35:31 +0300

    Implement the next step - open the editor and warm up TextMesh Pro

--- Assets/Extra/Test/Scripts/Editor/MultiversionTests.cs
+++ Assets/Extra/Test/Scripts/Editor/MultiversionTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -11,13 +12,19 @@ public static class MultiversionTests {
 
     [MenuItem("Tools/Soft Mask/Run Multiversion Tests")]
     public static void Run() {
-        EditorUtility.DisplayProgressBar("Running multiversion tests", "Cloning project", 0.5f);
+        EditorUtility.DisplayProgressBar("Running multiversion tests", "Cloning project...", 0.25f);
         try {
             var projectPath = Directory.GetCurrentDirectory();
             var automationProjectPath = Path.Combine(projectPath, Path.Combine(AutomationProjectsRoot, "2019"));
             MakeCreanDirectory(automationProjectPath);
-            CopySubfolder(projectPath, automationProjectPath, "Assets");
+            CopySubfolder(projectPath, automationProjectPath, "Assets", excludeDirs: "TextMesh Pro");
             CopySubfolder(projectPath, automationProjectPath, "ProjectSettings");
+            
+            EditorUtility.DisplayProgressBar("Running multiversion tests", "Running editor...", 0.5f);
+            var editorArguments = string.Format("-projectPath {0} -batchmode -accept-apiupdate -quit -executeMethod MultiversionTests.ClientSideRun", automationProjectPath);
+            var editor = Process.Start(Unity2019EditorPath, editorArguments);
+            editor.WaitForExit();
+
         } finally {
             EditorUtility.ClearProgressBar();
         }
@@ -29,19 +36,29 @@ public static class MultiversionTests {
         Directory.CreateDirectory(automationProjectDir);
     }
 
-
-    static void CopySubfolder(string from, string to, string subFolder) {
+    static void CopySubfolder(string from, string to, string subFolder, params string[] excludeDirs) {
         CopyFilesRecursively(
             new DirectoryInfo(Path.Combine(from, subFolder)),
-            new DirectoryInfo(Path.Combine(to, subFolder)));
+            new DirectoryInfo(Path.Combine(to, subFolder)),
+            excludeDirs);
     }
 
-    static void CopyFilesRecursively(DirectoryInfo from, DirectoryInfo to) {
+    static void CopyFilesRecursively(DirectoryInfo from, DirectoryInfo to, params string[] excludeDirs) {
         if (!to.Exists)
             to.Create();
         foreach (var dir in from.GetDirectories())
-            CopyFilesRecursively(dir, to.CreateSubdirectory(dir.Name));
+            if (!excludeDirs.Any(exc => dir.FullName.Contains(exc)))
+                CopyFilesRecursively(dir, to.CreateSubdirectory(dir.Name));
         foreach (var file in from.GetFiles())
             file.CopyTo(Path.Combine(to.FullName, file.Name));
+    }
+
+    public static void ClientSideRun() {
+    #if UNITY_2019_1_OR_NEWER
+        UnityEngine.Debug.Log("Import TMPro essentials");
+        TMPro.TMP_PackageUtilities.ImportProjectResourcesMenu();
+        UnityEngine.Debug.Log("Converting TMPro GUIDs");
+        TMPro.TMP_PackageUtilities.ConvertProjectGUIDsMenu();
+    #endif
     }
 }
