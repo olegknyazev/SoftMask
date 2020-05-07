d67b393c1702f05bde9fc9e2460ead24844a6b5d
Oleg Knyazev <knyazev751@gmail.com>
2020-05-07 11:44:11 +0300

    Add a template of test scene for Image's pixels per unit multiplier support

---
+++ Assets/Extra/Test/Scripts/Cases/TestImagePixelsPerUnitMultiplier.cs
@@ -0,0 +1,18 @@
+﻿#if UNITY_2019_2_OR_NEWER
+using System.Collections;
+#endif
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SoftMasking.Tests {
+    public class TestImagePixelsPerUnitMultiplier : MonoBehaviour {
+        public AutomatedTest automatedTest;
+        public Image image;
+
+    #if UNITY_2019_2_OR_NEWER
+        public IEnumerator Start() {
+            yield break;
+        }
+    #endif
+    }
+}
