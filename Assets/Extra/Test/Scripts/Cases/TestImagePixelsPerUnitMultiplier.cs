e6fcfdad066573c8296dfa01e71e5a92f20360de
Oleg Knyazev <knyazev751@gmail.com>
2020-05-08 13:10:31 +0300

    Add a test for the spritePixelsPerUnitMultiplier parameter

--- Assets/Extra/Test/Scripts/Cases/TestImagePixelsPerUnitMultiplier.cs
+++ Assets/Extra/Test/Scripts/Cases/TestImagePixelsPerUnitMultiplier.cs
@@ -1,26 +1,31 @@
-﻿#if UNITY_2019_2_OR_NEWER
-using System.Collections;
-#endif
+﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.UI;
 
 namespace SoftMasking.Tests {
     public class TestImagePixelsPerUnitMultiplier : MonoBehaviour {
         public AutomatedTest automatedTest;
         public Image image;
+        public SoftMask mask;
         public float[] pixelsPerUnitMultiplierSteps;
 
-    #if UNITY_2019_2_OR_NEWER
         public IEnumerator Start() {
+            Assert.IsTrue(image || mask);
             foreach (var multiplier in pixelsPerUnitMultiplierSteps) {
-                image.pixelsPerUnitMultiplier = multiplier;
-                // There is a bug in 2019.3: changing pixelsPerUnitMultiplier from script
-                // doesn't update anything.
-                image.SetVerticesDirty();
+            #if UNITY_2019_2_OR_NEWER
+                if (image) {
+                    image.pixelsPerUnitMultiplier = multiplier;
+                    // There is a bug in 2019.3: changing pixelsPerUnitMultiplier from script
+                    // doesn't update anything.
+                    image.SetVerticesDirty();
+                }
+            #endif
+                if (mask)
+                    mask.spritePixelsPerUnitMultiplier = multiplier;
                 yield return automatedTest.Proceed(2f);
             }
             yield return automatedTest.Finish();
         }
-    #endif
     }
 }
