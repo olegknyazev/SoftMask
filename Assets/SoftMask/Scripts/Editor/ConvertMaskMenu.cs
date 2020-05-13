f395078fbd0f37c8c6a89c8a74160eaed905cdb5
Oleg Knyazev <knyazev751@gmail.com>
2020-05-13 21:58:29 +0300

    Add a first and dirty version of a mask conversion tool

---
+++ Assets/SoftMask/Scripts/Editor/ConvertMaskMenu.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+namespace SoftMasking.Editor {
+    public static class ConvertMaskMenu {
+        [MenuItem("Tools/Soft Mask/Convert Mask to Soft Mask")]
+        public static void Convert() {
+            Assert.IsTrue(CanConvert());
+            var selectedTransforms = Selection.GetTransforms(SelectionMode.Editable);
+            foreach (var transform in selectedTransforms)
+                Convert(transform.gameObject);
+        }
+        
+        [MenuItem("Tools/Soft Mask/Convert Mask to Soft Mask", validate = true)]
+        public static bool CanConvert() {
+            var selectedTransforms = Selection.GetTransforms(SelectionMode.Editable);
+            return selectedTransforms.Any()
+                && selectedTransforms.All(t => IsConvertibleMask(t.gameObject));
+        }
+
+        static bool IsConvertibleMask(GameObject gameObject) {
+            var mask = gameObject.GetComponent<Mask>();
+            if (!mask)
+                return false;
+            var graphic = gameObject.GetComponent<Graphic>();
+            if (!graphic) 
+                return false;
+            return graphic is Image || graphic is RawImage;
+        }
+
+        static void Convert(GameObject gameObject) {
+            Assert.IsTrue(IsConvertibleMask(gameObject));
+            var mask = gameObject.GetComponent<Mask>();
+            var graphic = gameObject.GetComponent<Graphic>();
+            var softMask = gameObject.AddComponent<SoftMask>();
+            if (mask.showMaskGraphic)
+                softMask.source = SoftMask.MaskSource.Graphic;
+            else {
+                if (graphic is Image) {
+                    var image = (Image)graphic;
+                    softMask.source = SoftMask.MaskSource.Sprite;
+                    softMask.sprite = image.sprite;
+                    softMask.spriteBorderMode = BorderModeOf(image); // TODO check unsupported borderMode
+                #if UNITY_2019_2_OR_NEWER
+                    softMask.spritePixelsPerUnitMultiplier = image.pixelsPerUnitMultiplier;
+                #endif
+                    // TODO remove standard mask
+                } else if (graphic is RawImage) {
+                    var rawImage = (RawImage)graphic;
+                    softMask.source = SoftMask.MaskSource.Texture;
+                    if (rawImage.texture is Texture2D)
+                        softMask.texture = (Texture2D)rawImage.texture;
+                    else if (rawImage.texture is RenderTexture)
+                        softMask.renderTexture = (RenderTexture)rawImage.texture;
+                    else
+                        ; // TODO report error
+                    softMask.textureUVRect = rawImage.uvRect;
+                    // TODO remove standard mask
+                } else {
+                    Debug.LogAssertionFormat("Converted Game Object should have an Image or Raw Image component");
+                }
+            }
+            //Undo.RecordObjects();
+        }
+
+        // TODO copied from SoftMask.cs
+        static SoftMask.BorderMode BorderModeOf(Image image) {
+            switch (image.type) {
+                case Image.Type.Simple: return SoftMask.BorderMode.Simple;
+                case Image.Type.Sliced: return SoftMask.BorderMode.Sliced;
+                case Image.Type.Tiled: return SoftMask.BorderMode.Tiled;
+                default:
+                    return SoftMask.BorderMode.Simple;
+            }
+        }
+    }
+}
+ 
---
+++ Assets/SoftMask/Scripts/Editor/ConvertMaskMenuTest.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+using UnityEngine.UI;
+
+namespace SoftMasking.Editor {
+    public class ConvertMaskMenuTest {
+        [Test] public void WhenNoObjectSelected_ShouldBeNotAvailable() {
+            Selection.objects = new Object [0];
+            Assert.IsFalse(ConvertMaskMenu.CanConvert());
+        }
+
+        [Test] public void WhenEmptyObjectSelected_ShouldBeNotAvailable() {
+            var go = new GameObject("EmptyTestObject");
+            Selection.objects = new [] { go };
+            Assert.IsFalse(ConvertMaskMenu.CanConvert());
+            Object.DestroyImmediate(go);
+        }
+
+        [Test] public void WhenConvertibleObjectsSelected_ShouldBeAvailable() {
+            var go = new GameObject("TestObject", typeof(Mask), typeof(Image));
+            Selection.objects = new [] { go };
+            Assert.IsTrue(ConvertMaskMenu.CanConvert());
+            Object.DestroyImmediate(go);
+        }
+
+        [Test] public void WhenInvokedOnNonRenderableImage_ShouldConvertItToSpriteSoftMask() {
+            var standardSprite = AssetDatabase.GetBuiltinExtraResource<Sprite>("UI/Skin/UISprite.psd");
+            var go = new GameObject("TestObject");
+            var image = go.AddComponent<Image>();
+            image.sprite = standardSprite;
+            image.type = Image.Type.Sliced;
+            var mask = go.AddComponent<Mask>();
+            mask.showMaskGraphic = false;
+            Selection.objects = new [] { go };
+            ConvertMaskMenu.Convert();
+            var softMask = go.GetComponent<SoftMask>();
+            Assert.AreEqual(standardSprite, softMask.sprite);
+            Assert.AreEqual(SoftMask.BorderMode.Sliced, softMask.spriteBorderMode);
+            // TODO check standard mask is removed (here or a seprate test?)
+            Object.DestroyImmediate(go);
+            // TODO use a `using` scope?
+        }
+    }
+}
