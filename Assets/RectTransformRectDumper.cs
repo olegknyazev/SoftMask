673d95d5f0e96c2184b0f472c33d00805f1e7274
Oleg Knyazev <knyazev751@gmail.com>
2020-09-26 11:06:15 +0300

    Add an experimental and non-test-covered-yet support for the preserve aspect ratio image option

---
+++ Assets/RectTransformRectDumper.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(RectTransform))]
+public class RectTransformRectDumper : MonoBehaviour {
+    public void Update() {
+        Debug.Log(GetComponent<RectTransform>().rect);
+    }
+}
