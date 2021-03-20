e1406cd99eaed040b467bdb75b66d3c32d8534a0
Oleg Knyazev <knyazev751@gmail.com>
2021-03-20 13:02:44 +0300

    Add a test scene for case when DestroyImmediate was called from a physics callback
    The test doesn't actually test yet because the current ILogHandler-based
    log interception mechanism does not work for improper DestroyImmediate
    use message.

---
+++ Assets/Extra/Test/Scripts/Cases/TestDisableFromPhysicsCallback.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace SoftMasking.Tests {
+    // This test reproduces a long-existing Soft Mask issue: if SoftMaskable go out
+    // of SoftMask in physics callback, it causes an error because DestroyImmediate
+    // should not be called from physics callbacks.
+    // See SoftMaskable.FindMaskOrDie.
+    public class TestDisableFromPhysicsCallback : MonoBehaviour {
+        public Transform childToUnparent;
+
+        public AutomatedTest automatedTest;
+
+        public void Start() {
+            automatedTest.Proceed();
+        }
+
+        void OnCollisionEnter(Collision other) {
+            childToUnparent.SetParent(childToUnparent.parent.parent);
+            StartCoroutine(ProceedAndFinish());
+        }
+
+        IEnumerator ProceedAndFinish() {
+            yield return automatedTest.Proceed(0.5f);
+            yield return automatedTest.Finish();
+        }
+    }
+}
