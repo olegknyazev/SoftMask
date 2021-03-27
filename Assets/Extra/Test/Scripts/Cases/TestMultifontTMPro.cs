77705b21e0df71ce16982d1707ea49946bba55e9
Oleg Knyazev <knyazev751@gmail.com>
2021-03-27 13:30:20 +0300

    Add a test case that reproduces the multiline TMPro issue

---
+++ Assets/Extra/Test/Scripts/Cases/TestMultifontTMPro.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace SoftMasking.Tests {
+    public class TestMultifontTMPro : MonoBehaviour {
+        public TextMeshProUGUI textMeshPro;
+        [Multiline] public string textToApply; 
+            
+        public AutomatedTest automatedTest;
+
+        public IEnumerator Start() {
+            yield return automatedTest.Proceed();
+            textMeshPro.text = textToApply;
+            yield return automatedTest.Proceed();
+            yield return automatedTest.Finish();
+        }
+    }
+}
