a8760299d6b16b380785ef51460513100a86fe73
Oleg Knyazev <knyazev751@gmail.com>
2021-03-27 14:10:52 +0300

    Add a test for preserve aspect ratio support

---
+++ Assets/Extra/Test/Scripts/Cases/TestPreserveAspectRatio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SoftMasking.Tests {
+    public class TestPreserveAspectRatio : MonoBehaviour {
+        public Image image;
+        public AutomatedTest automatedTest;
+
+        public IEnumerator Start() {
+            while (true) {
+                foreach (var preserve in new[] { false, true, false, true }) {
+                    image.preserveAspect = preserve;
+                    yield return automatedTest.Proceed(0.5f);
+                }
+                yield return automatedTest.Finish();
+            }
+        }
+    }
+}
--- Assets/RectTransformRectDumper.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-[ExecuteInEditMode]
-[RequireComponent(typeof(RectTransform))]
-public class RectTransformRectDumper : MonoBehaviour {
-    public void Update() {
-        Debug.Log(GetComponent<RectTransform>().rect);
-    }
-}
