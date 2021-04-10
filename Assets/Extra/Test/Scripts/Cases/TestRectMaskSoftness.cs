1960cd655fdb1364e8d9729a8cff89da58e84362
Oleg Knyazev <knyazev751@gmail.com>
2021-04-10 16:12:38 +0300

    Add a test for RectMask2D's MaskSoftness parameter support

---
+++ Assets/Extra/Test/Scripts/Cases/TestRectMaskSoftness.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SoftMasking.Tests {
+    public class TestRectMaskSoftness : MonoBehaviour {
+        public RectMask2D rectMask;
+        public AutomatedTest automatedTest;
+
+        public IEnumerator Start() {
+        #if UNITY_2020_1_OR_NEWER
+            while (true) {
+                foreach (var softness in new[] { 0, 5, 25 }) {
+                    rectMask.softness = Vector2Int.one * softness;
+                    yield return automatedTest.Proceed(1f);
+                }
+                yield return automatedTest.Finish();
+            }
+        #else
+                Debug.LogAssertion("This test should not be run in Unity prior to 2020");
+        #endif
+        }
+    }
+}
