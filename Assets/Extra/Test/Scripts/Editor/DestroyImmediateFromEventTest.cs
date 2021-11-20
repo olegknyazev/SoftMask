6e39860ab76a557d360f46a81513bc836bdff432
Oleg Knyazev <knyazev751@gmail.com>
2021-11-20 17:26:51 +0300

    Add a test that reproduces one of the causes of "Destroying object multiple times"

---
+++ Assets/Extra/Test/Scripts/Editor/DestroyImmediateFromEventTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace SoftMasking.Editor {
+    public class DestroyImmediateFromEventTest {
+        [UnityTest]
+        public IEnumerator Simultaneous_deletion_of_SoftMaskable_and_OnCanvasHierarchyChanged_should_not_spam_errors() {
+            var root = new GameObject("Canvas", typeof(Canvas));
+
+            var mask = new GameObject("Mask", typeof(SoftMask));
+            mask.transform.SetParent(root.transform);
+
+            var canvasObject = new GameObject("Canvas", typeof(Canvas));
+            canvasObject.GetComponent<Canvas>().overrideSorting = true;
+            canvasObject.transform.SetParent(mask.transform);
+            canvasObject.SetActive(false);
+
+            yield return null;
+
+            Object.DestroyImmediate(mask.GetComponent<SoftMask>());
+
+            yield return null;
+
+            Object.DestroyImmediate(canvasObject);
+        }
+    }
+}
