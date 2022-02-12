23d0a8f97cca4065ede64d0b0d3bec1aacf7ccf9
Oleg Knyazev <knyazev751@gmail.com>
2022-02-12 10:12:10 +0300

    Make the previous test actually reproduce the issue

---
+++ Assets/Extra/Test/Scripts/Cases/TextMeshPro/TestEnablingWithSubMesh.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using SoftMasking.Tests;
+using TMPro;
+
+namespace SoftMasking.TextMeshPro.Tests {
+    public class TestEnablingWithSubMesh : MonoBehaviour {
+        public TextMeshProUGUI textMesh;
+        public string textToSet;
+        public AutomatedTest automatedTest;
+
+        public IEnumerator Start() {
+            yield return automatedTest.Proceed(1f);
+            textMesh.text = textToSet;
+            textMesh.gameObject.SetActive(true);
+            yield return automatedTest.Proceed(1f);
+            yield return automatedTest.Finish();
+        }
+    }
+}
